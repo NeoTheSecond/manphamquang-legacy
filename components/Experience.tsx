b4e79ef6137393ec9233814a3814ed73f29b1e0c
Man Pham Quang <quangman1404@gmail.com>
2023-02-01 13:16:33 +0700

    added styles to index page

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,21 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { ExperienceType } from "../types";
 import Section from "./Section";
 import Image from "next/image";
+import { RxCaretDown } from "react-icons/rx";
+import classNames from "classnames";
 
 const ExperienceCard = ({ data }: { data: ExperienceType }) => {
+  const [isShow, setIsShow] = useState(false);
+  const handleClick = () => {
+    setIsShow(!isShow);
+  };
   return (
-    <div className="flex mb-2">
+    <div
+      className={classNames(
+        "flex mb-2 bg-slate-200 border-slate-300 border rounded cursor-pointer transition ease-in-out delay-150",
+        isShow ? "h-[100%]" : "h-[74px]"
+      )}
+      onClick={handleClick}
+    >
       <Image
         src={data.cover_image.publicUrl}
         alt={data.title}
         width={70}
         height={70}
+        className="flex-initial h-[70px]"
       />
-      <div className="ml-2 text-zinc-800">
-        <div className="font-medium">{data.title}</div>
-        <div className="font-light">{data.duration}</div>
-        <div className="font-light">{data.location}</div>
+      <div className="ml-2 text-zinc-800 flex-auto select-text">
+        <div className="font-medium flex justify-between items-center">
+          {data.title}
+          <RxCaretDown className="mr-1" />
+        </div>
+        <div className="font-light">
+          {data.location} - {data.duration}
+        </div>
+        <div className={classNames({ "line-clamp-1": !isShow })}>
+          {data.description}
+        </div>
       </div>
     </div>
   );
@@ -24,7 +44,7 @@ const ExperienceCard = ({ data }: { data: ExperienceType }) => {
 export default function Experience({ data }: { data: Array<ExperienceType> }) {
   return (
     <Section className="dark:text-white">
-      <h2>Experience</h2>
+      <h4 className="text-4xl text-zinc-700 mb-2">Experience</h4>
       <div>
         {data.map((experience: ExperienceType) => (
           <ExperienceCard data={experience} key={experience.id} />
--- pages/index.tsx
+++ pages/index.tsx
@@ -10,40 +10,75 @@ import { EducationType, ExperienceType } from "../types";
 import { InferGetStaticPropsType } from "next";
 import { GetStaticProps } from "next";
 import Experience from "../components/Experience";
+import {
+  AiOutlineHome,
+  AiOutlineMail,
+  AiOutlineFacebook,
+  AiOutlineInstagram,
+  AiOutlineLinkedin,
+} from "react-icons/ai";
 
 interface Props {
   educations: Array<EducationType>;
   experiences: Array<ExperienceType>;
 }
 
+const Info = ({ icon, text }: { icon: React.ReactElement; text: string }) => (
+  <div className="flex space-x-2 items-center">
+    {icon} <p>{text}</p>
+  </div>
+);
+
 const Home: NextPage<Props> = ({ educations, experiences }) => {
   return (
     <>
       <Section className="flex">
-        <Image
-          src="/portrait.jpg"
-          width={220}
-          height={220}
-          alt="Portrait of me"
-          quality={100}
-          placeholder="blur"
-          blurDataURL="/portrait.jpg"
-          className="rounded-full"
-        />
-        <div className="ml-4">
-          <div>Based in Vietnam</div>
-          <div>quangman1404@gmail.com</div>
-          <div>Facebook</div>
-          <div>Instagram</div>
-          <div>Linkedin</div>
+        <div className="text-center">
+          <Image
+            src="/portrait.jpg"
+            width={220}
+            height={220}
+            alt="Portrait of me"
+            quality={100}
+            placeholder="blur"
+            blurDataURL="/portrait.jpg"
+            className="rounded-full"
+          />
+          <div className="text-2xl font-bold">Mẫn Phạm</div>
+          <div className="font-light">Web Developer</div>
+        </div>
+        <div className="ml-4 space-y-3">
+          <Info
+            icon={<AiOutlineHome className="h-6 w-6" />}
+            text="Based in Vietnam"
+          />
+          <Info
+            icon={<AiOutlineMail className="h-6 w-6" />}
+            text="quangman1404@gmail.com"
+          />
+          <Info
+            icon={<AiOutlineFacebook className="h-6 w-6" />}
+            text="Facebook"
+          />
+          <Info
+            icon={<AiOutlineInstagram className="h-6 w-6" />}
+            text="Instagram"
+          />
+          <Info
+            icon={<AiOutlineLinkedin className="h-6 w-6" />}
+            text="Linkedin"
+          />
         </div>
       </Section>
-      <Section>
-        <h4>👋🏻 Hello!</h4>
-        <p>
+      <Section className="text-xl">
+        <h4>
+          <span className="text-3xl">👋🏻</span> Hello!
+        </h4>
+        <p className="font-light">
           My name is Mẫn, a Fullstack Web Developer with over 5 years of
           experience. I am also an undergraduate enrolled in Bachelor of
           Information Technology at RMIT University in Vietnam. <br />
+          <br />
           Aside from the web, I have dabbled in many other tech fields like
           Machine Learning, Penetration Testing, Video Games, etc. The more
           about my work can be found in experience.
