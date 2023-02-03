8f42a3573949d19166c0854317a3fd34ff7dd61c
Man Pham Quang <quangman1404@gmail.com>
2023-02-03 20:41:59 +0700

    styled toast

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,10 @@
-import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "../components/Layout";
 import { ApolloProvider } from "@apollo/react-hooks";
 import client from "../apollo-client";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import "../styles/globals.css";
 
 // pages/_app.js
 import {
@@ -26,8 +26,19 @@ const azeret = Azeret_Mono({ subsets: ["latin"] });
 
 const archivo = Archivo({ subsets: ["latin"] });
 
+const contextClass = {
+  success: "bg-blue-600",
+  error: "bg-red-600",
+  info: "bg-gray-600",
+  warning: "bg-orange-400",
+  default: "bg-slate-400 text-gray-900",
+  dark: "border border-yellow-300 bg-slate-900 text-yellow-300",
+};
+
 function MyApp({ Component, pageProps }: AppProps) {
   // On page load or when changing themes, best to add inline in `head` to avoid FOUC
+  type ToastTheme = "light" | "dark";
+  let toastTheme: ToastTheme = "light";
   if (typeof window !== "undefined") {
     if (
       localStorage.theme === "dark" ||
@@ -35,8 +46,10 @@ function MyApp({ Component, pageProps }: AppProps) {
         window.matchMedia("(prefers-color-scheme: dark)").matches)
     ) {
       document.documentElement.classList.add("dark");
+      toastTheme = "dark";
     } else {
       document.documentElement.classList.remove("dark");
+      toastTheme = "light";
     }
   }
   return (
@@ -49,7 +62,27 @@ function MyApp({ Component, pageProps }: AppProps) {
       <ApolloProvider client={client}>
         <Layout>
           <Component {...pageProps} />
-          <ToastContainer autoClose={1300} hideProgressBar />
+          <ToastContainer
+            toastClassName={(data) => {
+              if (data) {
+                console.log(data.type);
+
+                return (
+                  contextClass[
+                    toastTheme === "dark" ? "dark" : data.type || "default"
+                  ] +
+                  " relative flex p-1 min-h-10 shadow-xl mb-2 rounded justify-between overflow-hidden cursor-pointer"
+                );
+              } else {
+                return "relative flex p-1 min-h-10 shadow-xl rounded justify-between overflow-hidden cursor-pointer";
+              }
+            }}
+            bodyClassName={() => "text-md block p-3"}
+            autoClose={false}
+            hideProgressBar
+            theme={toastTheme}
+            draggable
+          />
         </Layout>
       </ApolloProvider>
     </>
