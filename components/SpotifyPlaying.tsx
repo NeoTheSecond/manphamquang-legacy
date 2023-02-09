47580161ea7c9d69ef0fb594049090c1e1015ae5
Man Pham Quang <quangman1404@gmail.com>
2023-02-09 15:36:04 +0700

    added scrolling text

--- components/SpotifyPlaying.tsx
+++ components/SpotifyPlaying.tsx
@@ -1,6 +1,7 @@
 import useSWR from "swr";
 import { BsSpotify } from "react-icons/bs";
 import Image from "next/image";
+import Marquee from "react-fast-marquee";
 
 const SPOTIFY_URL = "https://api.spotify.com/v1/me/player/currently-playing";
 
@@ -26,14 +27,34 @@ export default function SpotifyPlaying({
 
   //   if (!error || !isLoading) {
   return (
-    <div className="p-3 border rounded border-cyan-300">
-      <BsSpotify /> {data.item.name}
-      <Image
-        width={64}
-        height={64}
-        src={data.item.album.images[2].url}
-        alt={data.item.name}
-      />
+    <div className="w-full p-3 border rounded md:w-80 border-emerald-300 text-emerald-400">
+      <div className="flex items-center space-x-1">
+        <BsSpotify className="w-5 h-5" />{" "}
+        <p className="text-xl">Listening to:</p>
+      </div>
+      <div className="flex space-x-2">
+        <Image
+          className="flex-none"
+          width={64}
+          height={64}
+          src={data.item.album.images[2].url}
+          alt={data.item.name}
+        />
+        <div>
+          <Marquee
+            gradient={false}
+            delay={2}
+            pauseOnHover
+            style={{
+              background: "transparent",
+              height: "fit-content",
+            }}
+          >
+            <div className="font-bold"> {data.item.name}</div>
+          </Marquee>
+          <div className="font-semibold">{data.item.album.name}</div>
+        </div>
+      </div>
     </div>
   );
   //   }
