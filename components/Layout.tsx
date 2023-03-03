f1350bfbbbeaa29d7886924c9615218bfcdedde3
Man Pham Quang <quangman1404@gmail.com>
2023-03-03 19:20:34 +0700

    test full screen height content

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -5,9 +5,9 @@ import Footer from "./Footer";
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="min-h-screen dark:bg-slate-900 dark:text-cyan-50 min-w-fit">
-      <main className="p-8 m-auto  md:p-0 md:pt-8 md:max-w-screen-md">
+      <main className="h-full p-8 m-auto md:p-0 md:pt-8 md:max-w-screen-md">
         <Header />
-        <div className="px-5 py-2 border-2 shadow-lg dark:border-cyan-300 dark:shadow-cyan-500/50 ">
+        <div className="min-h-[calc(100vh-(2rem+52px+8px+48px))] px-5 py-2 border-2 shadow-lg dark:border-cyan-300 dark:shadow-cyan-500/50 ">
           {children}
         </div>
         <Footer />
