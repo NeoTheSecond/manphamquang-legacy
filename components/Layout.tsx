8b4659789a5de6285bf0925c796b15210bff3110
Man Pham Quang <quangman1404@gmail.com>
2023-03-03 16:17:20 +0700

    add basic styles

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -4,10 +4,10 @@ import Footer from "./Footer";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="dark:bg-slate-900 dark:text-cyan-50 min-h-screen min-w-fit">
-      <main className="p-8 md:p-0 md:pt-8 md:max-w-screen-md m-auto">
+    <div className="min-h-screen dark:bg-slate-900 dark:text-cyan-50 min-w-fit">
+      <main className="p-8 m-auto  md:p-0 md:pt-8 md:max-w-screen-md">
         <Header />
-        <div className="py-2 px-5 border-2 dark:border-cyan-300 shadow-lg dark:shadow-cyan-500/50 ">
+        <div className="px-5 py-2 border-2 shadow-lg dark:border-cyan-300 dark:shadow-cyan-500/50 ">
           {children}
         </div>
         <Footer />
--- pages/blogs/index.tsx
+++ pages/blogs/index.tsx
@@ -1,5 +1,73 @@
-import React from "react";
+import Image from "next/image";
+import React, { ReactElement } from "react";
+
+const TableHead = ({ children }: { children?: string }) => (
+  <th className="p-2 border-b border-slate-300">{children}</th>
+);
+
+const TableData = ({ children }: { children: string | ReactElement }) => (
+  <td className="p-2 border-b border-slate-300">{children}</td>
+);
+
+const data = [
+  {
+    title: "How I build my website",
+    type: "technical",
+    duration: "5 min",
+    postedOn: "03/03/2023",
+  },
+  {
+    title: "How I build a better portfolio",
+    type: "fun",
+    duration: "10 min",
+    postedOn: "10/03/2023",
+  },
+  {
+    title: "Practicing guitar is fun",
+    type: "fun",
+    duration: "3 min",
+    postedOn: "24/04/2023",
+  },
+];
 
 export default function Blogs() {
-  return <div>show some blogs</div>;
+  return (
+    <div>
+      <h1 className="text-4xl">Blogs</h1>
+      <p>Where I write stuff</p>
+      <table className="w-full text-left table-auto">
+        <thead>
+          <tr>
+            <TableHead></TableHead>
+            <TableHead>Title</TableHead>
+            <TableHead>Type</TableHead>
+            <TableHead>duration</TableHead>
+            <TableHead>posted on</TableHead>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((row) => (
+            <tr key={row.title} className="cursor-pointer hover:bg-slate-200">
+              <TableData>
+                <Image
+                  src="/portrait.jpg"
+                  width={50}
+                  height={30}
+                  alt="Portrait of me"
+                  quality={100}
+                  placeholder="blur"
+                  blurDataURL="/portrait.jpg"
+                  className="rounded-sm"
+                />
+              </TableData>
+              <TableData>{row.title}</TableData>
+              <TableData>{row.type}</TableData>
+              <TableData>{row.duration}</TableData>
+              <TableData>{row.postedOn}</TableData>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
